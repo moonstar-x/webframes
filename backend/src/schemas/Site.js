d410f2435b4ee3a009d1e28125fae03a97ec4b59
moonstar-x <christian.lopez99@outlook.com>
2021-05-29 19:41:50 -0500

    Created Site schema.

---
+++ backend/src/schemas/Site.js
@@ -0,0 +1,20 @@
+const Joi = require('joi');
+
+const CreateSchema = Joi.object({
+  id: Joi.forbidden(),
+  name: Joi.string().trim().required(),
+  url: Joi.string().uri().required(),
+  image: Joi.string().base64().allow(null).allow('').default(null)
+});
+
+const UpdateSchema = Joi.object({
+  id: Joi.forbidden(),
+  name: Joi.string().trim(),
+  url: Joi.string().uri(),
+  image: Joi.string().base64()
+});
+
+module.exports = {
+  SiteCreateSchema: CreateSchema,
+  SiteUpdateSchema: UpdateSchema
+};
