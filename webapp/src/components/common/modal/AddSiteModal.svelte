4493ba7cf7313d63ddb7902a7f53f9957e28b989
moonstar-x <christian.lopez99@outlook.com>
2021-05-31 15:15:21 -0500

    Added modal components.

---
+++ webapp/src/components/common/modal/AddSiteModal.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+  import Modal from './Modal.svelte';
+
+  const dispatch = createEventDispatcher();
+
+  const handleCloseButtonClick = () => {
+    dispatch('close');
+  };
+
+  export let show;
+</script>
+
+<style>
+  h3 {
+    margin: 0;
+  }
+</style>
+
+<Modal {show} withClose on:close>
+  <svelte:fragment slot="modal-header">
+    <h3>Add a new site</h3>
+  </svelte:fragment>
+  <svelte:fragment slot="modal-content">
+    
+  </svelte:fragment>
+  <svelte:fragment slot="modal-footer">
+    <button on:click={handleCloseButtonClick}>CLOSE</button>
+  </svelte:fragment>
+</Modal>
---
+++ webapp/src/components/common/modal/Modal.svelte
@@ -0,0 +1,92 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher();
+
+  const handleClose = () => {
+    dispatch('close');
+  };
+
+  export let show;
+  export let id = null;
+  export let withClose = true;
+</script>
+
+<style>
+  .modal {
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    z-index: 100;
+    background-color: rgba(0, 0, 0, 0.4);
+  }
+
+  .modal-content {
+    min-height: 10vh;
+    max-height: 70vh;
+    overflow-y: scroll;
+    background-color: var(--bg-light-gray);
+    margin: 20vh auto 10vh auto;
+    padding: 0;
+    border: 1px solid #888;
+    border-radius: 10px;
+    width: 80%;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  }
+
+  .modal-header, .modal-footer {
+    padding: 1rem;
+    background-color: #333;
+    color: var(--text-over-dark);
+  }
+
+  .modal-header {
+    border-radius: 10px 10px 0 0;
+    border-bottom: 1px solid #888;
+  }
+
+  .modal-footer {
+    border-radius: 0 0 10px 10px;
+    border-top: 1px solid #888;
+  }
+
+  .modal-body {
+    padding: 1rem;
+  }
+
+  .close {
+    color: #aaa;
+    float: right;
+    font-size: 28px;
+    font-weight: bold;
+    line-height: 0.7;
+  }
+
+  .close:hover, .close:focus {
+    color: #fff;
+    text-decoration: none;
+    cursor: pointer;
+  }
+</style>
+
+{#if show}
+  <div {id} class="modal">
+    <div class="modal-content">
+      <div class="modal-header">
+        {#if withClose}
+          <span class="close" on:click={handleClose}>&times;</span>
+        {/if}
+        <slot name="modal-header" />
+      </div>
+      <div class="modal-body">
+        <slot name="modal-content" />
+      </div>
+      <div class="modal-footer">
+        <slot name="modal-footer" />
+      </div>
+    </div>
+  </div>
+{/if}
