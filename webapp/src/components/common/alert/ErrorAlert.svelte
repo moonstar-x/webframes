80cd5c06219bd2f15d407afc23a2ea3915c1cf71
moonstar-x <christian.lopez99@outlook.com>
2021-06-01 17:39:34 -0500

    Styled ErrorAlert.

--- webapp/src/components/common/alert/ErrorAlert.svelte
+++ webapp/src/components/common/alert/ErrorAlert.svelte
@@ -4,7 +4,12 @@
 
 <style>
   div {
-    
+    padding: 0.75rem 1.25rem;
+    border: 1px solid transparent;
+    border-radius: 0.25rem;
+    color: #721c24;
+    background-color: #f8d7da;
+    border-color: #f5c6cb;
   }
 </style>
 
