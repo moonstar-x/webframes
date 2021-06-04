7a30ab59e2e003dc39ba13af1d6b71aa6e9d2c26
moonstar-x <christian.lopez99@outlook.com>
2021-06-03 23:54:57 -0500

    Styled EmptyFrameHolder.

--- webapp/src/components/common/frameHolder/EmptyFrameHolder.svelte
+++ webapp/src/components/common/frameHolder/EmptyFrameHolder.svelte
@@ -1,20 +1,41 @@
 <style>
   .empty-frame {
-    position: relative;
     height: 100%;
     width: 100%;
+    background: #333;
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 
   .empty-frame-content {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translateX(-50%) translateY(-50%);
+    border-radius: 5px;
+    background: #fff;
+    border: 1px solid #ddd;
+    max-width: 90%;
+    min-width: 50%;
+    margin: 1rem;
+    padding: 1rem;
+    text-align: center;
+  }
+
+  .empty-frame-title {
+    font-size: 1.2em;
+    margin-bottom: 1rem;
+  }
+
+  .empty-frame-description {
+    font-size: 0.8em;
   }
 </style>
 
 <div class="empty-frame">
   <div class="empty-frame-content">
-    <span>Select something.</span>
+    <div class="empty-frame-title">
+      No frame to display
+    </div>
+    <div class="empty-frame-description">
+      Click on one of the items in the sidebar on the left to launch a frame. You can also add a new site to frame by clicking on the button below.
+    </div>
   </div>
 </div>
