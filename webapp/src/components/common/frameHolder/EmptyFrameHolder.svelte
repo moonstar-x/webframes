251fb5f1b084c100d510c50c242a9abed69d9e28
moonstar-x <christian.lopez99@outlook.com>
2021-06-04 15:12:47 -0500

    Added add site button in EmptyFrameHolder.

--- webapp/src/components/common/frameHolder/EmptyFrameHolder.svelte
+++ webapp/src/components/common/frameHolder/EmptyFrameHolder.svelte
@@ -1,3 +1,24 @@
+<script>
+  import Fa from 'svelte-fa/src/fa.svelte';
+  import { faPlus } from '@fortawesome/free-solid-svg-icons';
+  import AddSiteModal from '../modal/AddSiteModal.svelte';
+
+  let showModal = false;
+
+  const handleShowModal = () => showModal = true;
+  const handleCloseModal = () => showModal = false;
+</script>
+
+<style>
+  .button {
+    margin-top: 1rem;
+  }
+
+  .button span {
+    margin-left: 0.5rem;
+  }
+</style>
+
 <div class="empty-frame">
   <div class="empty-frame-content">
     <div class="empty-frame-title">
@@ -6,5 +27,10 @@
     <div class="empty-frame-description">
       Click on one of the items in the sidebar on the left to launch a frame. You can also add a new site to frame by clicking on the button below.
     </div>
+    <button class="button" on:click={handleShowModal}>
+      <Fa icon={faPlus} />
+      <span>Add a new site</span>
+    </button>
   </div>
 </div>
+<AddSiteModal show={showModal} on:close={handleCloseModal} />
