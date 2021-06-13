5cd3bc720749659296ff432e539a224febe286f5
moonstar-x <christian.lopez99@outlook.com>
2021-06-12 23:54:23 -0500

    Added url processing for * in FrameHolder.

--- webapp/src/components/common/frameHolder/FrameHolder.svelte
+++ webapp/src/components/common/frameHolder/FrameHolder.svelte
@@ -1,11 +1,17 @@
 <script>
   import EmptyFrameHolder from './EmptyFrameHolder.svelte';
+  import { processFrameURL } from '../../../utils';
 
   export let title = null;
   export let url = null;
   export let sidebarVisible = true;
-  
   export let allowFullscreen = true;
+
+  $: {
+    if (url) {
+      url = processFrameURL(url);
+    }
+  }
 </script>
 
 <div class="frame-holder" class:expand={!sidebarVisible}>
--- webapp/src/utils/index.js
+++ webapp/src/utils/index.js
@@ -16,3 +16,17 @@ export const imageToDataURI = (image) => {
 export const changePageTitle = (extra) => {
   document.title = extra ? `Webframes | ${extra}` : 'Webframes';
 };
+
+export const processFrameURL = (url) => {
+  const isWildcardPresent = url.match(/(https?:\/\/\*)/gi);
+
+  if (!isWildcardPresent) {
+    return url;
+  }
+
+  const protocol = url.substring(0, url.indexOf('://') + 3);
+  const portIndex = url.lastIndexOf(':');
+  const port = portIndex > -1 ? url.substring(portIndex) : '';
+
+  return `${protocol}${window.location.hostname}${port}`;
+};
