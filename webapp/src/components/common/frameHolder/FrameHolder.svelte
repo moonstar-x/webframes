b54b32b8fc5c2c191e8c8b989584d2be3786468b
moonstar-x <christian.lopez99@outlook.com>
2021-06-12 23:22:19 -0500

    Added allowfullscreen to FrameHolder.

--- webapp/src/components/common/frameHolder/FrameHolder.svelte
+++ webapp/src/components/common/frameHolder/FrameHolder.svelte
@@ -4,11 +4,13 @@
   export let title = null;
   export let url = null;
   export let sidebarVisible = true;
+  
+  export let allowFullscreen = true;
 </script>
 
 <div class="frame-holder" class:expand={!sidebarVisible}>
   {#if url}
-    <iframe id="frame" {title} src={url} />
+    <iframe id="frame" {title} src={url} allowfullscreen={allowFullscreen} />
   {:else}
     <EmptyFrameHolder />
   {/if}
